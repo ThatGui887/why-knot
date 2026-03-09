9dde85a0acd944367902bcb2fe665b9ed2e484b7
ThatGui887 <markopienaar2@gmail.com>
2026-03-09 10:29:06 +0200

    created our user table containing entry and meta data

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,3 +1,14 @@
 // Convex database schema: defines the shape of documents we store
 import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
+
+// Single \"users\" table holding all journal entries and their metadata
+export default defineSchema({
+  users: defineTable({
+    text: v.string(),
+    summary: v.string(),
+    mood: v.string(),
+    moodEmoji: v.string(),
+    createdAt: v.number(),
+  }),
+});
