f1f38829091d1d356a2db65650abd204ab13b241
ThatGui887 <markopienaar2@gmail.com>
2026-03-09 11:33:47 +0200

    fetch entry ID

--- app/page.tsx
+++ app/page.tsx
@@ -4,3 +4,4 @@
 import { useState, useCallback } from "react";
 import { useMutation, useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
+import type { Id } from "@/convex/_generated/dataModel";
