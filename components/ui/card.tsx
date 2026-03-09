d35d1cb1903d8194089c8c012ae03707930f2d01
ThatGui887 <markopienaar2@gmail.com>
2026-03-09 12:44:31 +0200

    added spacing again

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -2,6 +2,7 @@
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
+
 // Outer card container with border, background, and padding
 function Card({ className, ...props }: React.ComponentProps<"div">) {
     return (
@@ -97,3 +98,4 @@ function Card({ className, ...props }: React.ComponentProps<"div">) {
     CardDescription,
     CardContent,
   }
+  
