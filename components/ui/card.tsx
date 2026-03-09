64eeb1359ed20d7141b78e9d3cffec1a9f12277a
ThatGui887 <markopienaar2@gmail.com>
2026-03-09 12:42:32 +0200

    Add CardContent component for main card body with horizontal padding

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -65,3 +65,14 @@ function Card({ className, ...props }: React.ComponentProps<"div">) {
       />
     )
   }
+  
+  // Main body content of the card
+  function CardContent({ className, ...props }: React.ComponentProps<"div">) {
+    return (
+      <div
+        data-slot="card-content"
+        className={cn("px-6", className)}
+        {...props}
+      />
+    )
+  }
