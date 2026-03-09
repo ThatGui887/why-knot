a41dd6ea043362279f5e6b6ccf6a9847f3f9edb1
ThatGui887 <markopienaar2@gmail.com>
2026-03-09 13:16:57 +0200

    Add cn utility function to merge and resolve Tailwind class names

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,8 @@
 // Utility for combining Tailwind class names in a safe, readable way
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+
+// Merge conditional class names and let Tailwind-merge resolve conflicts
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
