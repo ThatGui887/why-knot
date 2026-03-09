e60499ddd1dea3ed0c7b80d48532ddb742f4e019
ThatGui887 <markopienaar2@gmail.com>
2026-03-09 12:55:51 +0200

    Create Convex client instance using environment variable URL

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,3 +2,6 @@
 "use client";
 
 import { ConvexProvider, ConvexReactClient } from "convex/react";
+
+// Create a single Convex client instance using the public URL from env
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
